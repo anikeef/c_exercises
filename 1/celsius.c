bdc2e5311002fa4e32c794e995f828d73568d268
Gennadiy <genaydzhan70@gmail.com>
2019-06-07 20:26:41 +0600

    Change celsius.c to use functions

--- 1/celsius.c
+++ 1/celsius.c
@@ -2,12 +2,17 @@
 #define LOWER -10
 #define UPPER 10
 
+float celsius_to_fahr(float celsius);
 /* Print Celsius to Fahrengeit table from -10C to 10C */
 int main() {
   float fahr;
-  for (int celsius = LOWER; celsius <= UPPER; celsius += 1) {
+  for (int celsius = LOWER; celsius <= UPPER; ++celsius) {
     fahr = (9.0/5.0) * celsius + 32;
     printf("%3d\t%3.1f\n", celsius, fahr);
   }
   return 0;
+}
+
+float celsius_to_fahr(float celsius) {
+  return (9.0/5.0) * celsius + 32;
 }
