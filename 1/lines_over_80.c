4ec0aa45f1dd8c1a50569906f03ec2a97ee3d3f6
Gennadiy <genaydzhan70@gmail.com>
2019-06-09 14:39:18 +0600

    Add lines_over_80

---
+++ 1/lines_over_80.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#define MAXLINE 1024
+
+int get_line(char line[], int limit);
+
+/* Print all the lines from the input that are longer than 80 symbols */
+
+int main() {
+  char line[MAXLINE];
+  int line_size;
+
+  while ((line_size = get_line(line, MAXLINE)) > 0) {
+    if (line_size > 80)
+      printf("%s", line);
+  }
+  return 0;
+}
+
+/* Copy the line from input to the line[] array. Return the size of that line */
+
+int get_line(char line[], int limit) {
+  int c;
+  int i = 0;
+
+  for (i = 0; (c = getchar()) != EOF && c != '\n' && i < limit - 1; ++i)
+    line[i] = c;
+  if (c == '\n') {
+    line[i] = c;
+    ++i;
+  }
+  line[i] = '\0';
+  return i;
+}
